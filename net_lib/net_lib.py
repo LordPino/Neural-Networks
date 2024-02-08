b2971a74c8156c7ca5bcba4f679ecfd9f1ab6eed
Francesco Orlando <francesco.orlando270@gmail.com>
2024-02-08 15:38:45 +0100

    Add utility functions and neural network functions

--- net_lib/net_lib.py
+++ net_lib/net_lib.py
@@ -4,10 +4,68 @@ from matplotlib import pyplot as plt
 from enum import Enum
 from typing import List, Callable, Tuple
 
+# Enumeration for different loss functions
 class FunctionError(Enum):
-    MSE = 1
-    CROSS_ENTROPY = 2
+    MSE = 1 # Mean Squared Error
+    CROSS_ENTROPY = 2 # Cross Entropy Loss
 
+############################################
+# Utility functions
+# One hot encoding
+def one_hot(y: np.ndarray) -> np.ndarray:
+    one_hot_y = np.zeros((y.size, y.max() + 1))
+    one_hot_y[np.arange(y.size), y] = 1
+    one_hot_y = one_hot_y.T
+    
+    return one_hot_y
+
+# Generic derivative of a function
+def function_derivative(
+    function: Callable[[np.ndarray], np.ndarray], 
+    x: np.ndarray, 
+    dx: float = 1e-6
+) -> np.ndarray:
+    return (function(x + dx) - function(x)) / dx
+
+# Derivative of the cross entropy loss function
+def derivatie_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    m = y_true.shape[1]
+    loss = -np.sum(y_true * np.log(y_pred + 1e-9)) / m  # Aggiunto 1e-9 per la stabilità numerica
+    return loss
+
+# Derivative of the mean squared error loss function
+def derivative_mse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    return 2 * (y_pred - y_true) / y_true.size
+
+# Softmax function
+def soft_max(z: np.ndarray) -> np.ndarray:
+    return np.exp(z) / sum(np.exp(z))
+
+# Derivative of the softmax function
+def soft_max_derivative(z: np.ndarray) -> np.ndarray:
+    return soft_max(z) * (1 - soft_max(z))
+
+# ReLU function
+def ReLU(z: np.ndarray) -> np.ndarray:
+    return np.maximum(0, z)
+
+# Derivative of the ReLU function
+def ReLU_derivative(z: np.ndarray) -> np.ndarray:
+    return np.where(z > 0, 1, 0)
+
+# Returns the indices of the maximum values along an axis.
+def get_predictions(a: np.ndarray) -> np.ndarray:
+    return np.argmax(a, axis=0)
+
+# Accuracy function
+def get_accuracy(predictions: np.ndarray, y: np.ndarray) -> float:
+    print(predictions, y)
+    return np.sum(predictions == y) / y.size
+
+
+############################################
+# Neural Network functions
+# Initialize weights and biases
 def init_params(neurons_per_layer: List[int], output_val: int) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     if len(neurons_per_layer) < 2:
         raise ValueError("Number of layers must be at least 2")
@@ -30,6 +88,7 @@ def init_params(neurons_per_layer: List[int], output_val: int) -> Tuple[List[np.
 
     return weights, biases
 
+# Forward propagation
 def forward_prop(
     x: np.ndarray, 
     weights: List[np.ndarray], 
@@ -57,28 +116,7 @@ def forward_prop(
 
     return a, z
 
-def one_hot(y: np.ndarray) -> np.ndarray:
-    one_hot_y = np.zeros((y.size, y.max() + 1))
-    one_hot_y[np.arange(y.size), y] = 1
-    one_hot_y = one_hot_y.T
-    
-    return one_hot_y
-
-def function_derivative(
-    function: Callable[[np.ndarray], np.ndarray], 
-    x: np.ndarray, 
-    dx: float = 1e-6
-) -> np.ndarray:
-    return (function(x + dx) - function(x)) / dx
-
-def derivatie_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-    m = y_true.shape[1]
-    loss = -np.sum(y_true * np.log(y_pred + 1e-9)) / m  # Aggiunto 1e-9 per la stabilità numerica
-    return loss
-
-def derivative_mse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-    return 2 * (y_pred - y_true) / y_true.size
-
+# Back propagation
 def back_prop(
     y: np.ndarray, 
     z: List[np.ndarray], 
@@ -122,6 +160,7 @@ def back_prop(
 
     return dW, dB
 
+# Update weights and biases
 def update_params(
     weights: List[np.ndarray], 
     biases: List[np.ndarray], 
@@ -138,13 +177,7 @@ def update_params(
 
     return new_weights, new_biases
 
-def get_predictions(a: np.ndarray) -> np.ndarray:
-    return np.argmax(a, axis=0)
-
-def get_accuracy(predictions: np.ndarray, y: np.ndarray) -> float:
-    print(predictions, y)
-    return np.sum(predictions == y) / y.size
-
+# Gradient descent
 def gradint_descent(
     X: np.ndarray, 
     Y: np.ndarray, 
@@ -161,26 +194,31 @@ def gradint_descent(
     weights, biases = init_params(neurons_per_layer, output_val)
 
     for i in range(epochs):
-        a, z = forward_prop(X, weights, biases, activation_functions, output_function)
-        dW, dB = back_prop(Y, z, a, weights, biases, activation_functions, activation_derivatives, function_error, use_softmax)
-        weights, biases = update_params(weights, biases, dW, dB, learning_rate)
+        a, z = forward_prop(x=X, 
+                            weights=weights, 
+                            biases=biases, 
+                            activation_functions=activation_functions, 
+                            output_function=output_function)
+        dW, dB = back_prop(y=Y, 
+                           z=z, 
+                           a=a, 
+                           weights=weights, 
+                           biases=biases, 
+                           activation_functions=activation_functions, 
+                           activation_derivatives=activation_derivatives, 
+                           function_error=function_error, 
+                           use_softmax=use_softmax)
+        weights, biases = update_params(
+            weights=weights, 
+            biases=biases, 
+            dW=dW, 
+            dB=dB, 
+            learning_rate=learning_rate)
         if i % 10 == 0:
             print("epoch: ", i)
             print("Accuracy: ", get_accuracy(get_predictions(a[-1]), Y))
 
     return weights, biases
-
-def soft_max(z: np.ndarray) -> np.ndarray:
-    return np.exp(z) / sum(np.exp(z))
-
-def soft_max_derivative(z: np.ndarray) -> np.ndarray:
-    return soft_max(z) * (1 - soft_max(z))
-
-def ReLU(z: np.ndarray) -> np.ndarray:
-    return np.maximum(0, z)
-
-def ReLU_derivative(z: np.ndarray) -> np.ndarray:
-    return np.where(z > 0, 1, 0)
 
 data = pd.read_csv(r'.\test_data\train.csv')
 data = np.array(data)
@@ -201,10 +239,10 @@ W, B = gradint_descent(X=X_train,
                        Y=Y_train, 
                        epochs=500, 
                        learning_rate=0.1, 
-                       neurons_per_layer=[784, 15, 10],
+                       neurons_per_layer=[784, 10],
                        output_val=10, 
-                       activation_functions=[ReLU, ReLU], 
-                       activation_derivatives=[ReLU_derivative, ReLU_derivative], 
+                       activation_functions=[ReLU], 
+                       activation_derivatives=[ReLU_derivative], 
                        output_function=soft_max, 
                        function_error=FunctionError.CROSS_ENTROPY, 
                        use_softmax=True)
